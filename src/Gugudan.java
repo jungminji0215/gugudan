2ca8143f9259ad3e576e0092888182f0bdb2a9be
jungminji <wjdalswl0215@gmail.com>
2022-01-12 05:45:14 +0900

    조건문 사용

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -2,8 +2,8 @@ import java.util.Scanner;
 
 public class Gugudan {
     public static void main(String[] args) {
+        /*
         int dan = 1;
-
         while(dan < 10){
             System.out.println(dan + "단 출력----------");
             for(int i = 1; i < 10; i++) {
@@ -11,6 +11,24 @@ public class Gugudan {
             }
             dan ++;
         }
+         */
+
+        System.out.println("구구단 중 출력할 단은? : ");
+        Scanner scanner = new Scanner(System.in);
+        int dan = scanner.nextInt();
+
+        if (dan < 2) {
+            System.out.println("2이상, 9이하의 값만 입력할 수 있습니다.");
+        } else if (dan > 9) {
+            System.out.println("2이상, 9이하의 값만 입력할 수 있습니다.");
+        } else {
+
+                for(int i = 1; i < 10; i++) {
+                    System.out.println(dan * i);
+                }
+
+        }
+
 
     }
 }
