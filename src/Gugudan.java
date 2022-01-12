0d5948f569489604f09b8b3b481c869ea9275e39
jungminji <wjdalswl0215@gmail.com>
2022-01-12 09:06:04 +0900

    메소드 활용

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -1,13 +1,30 @@
 public class Gugudan {
     public static void main(String[] args) {
-        int[] gugudan = new int[9];
-
-        for(int i = 0; i < gugudan.length; i++){
-            gugudan[i] = 2 * (i+1);
-        }
-
-        for(int i = 0; i < gugudan.length; i++){
-            System.out.println(gugudan[i]);
+        for (int j = 2; j < 10; j++) {
+            // calculate와 print 메소드 활용해 구구단 구현하기
+            System.out.println("-------------" + j + "단-------------");
+            int[] result = calculate(j);
+            print(result);
         }
     }
+
+    public static int[] calculate(int times) {
+        int[] result = new int[9];
+
+        for (int i = 0; i < result.length; i++) {
+            // 계산 결과를 배열에 저장
+            result[i] = times * (i + 1);
+        }
+
+        return result;
+    }
+
+    public static void print(int[] result) {
+        for (int i = 0; i < result.length; i++) {
+            // 계산 결과를 화면에 출력
+            System.out.println(result[i]);
+        }
+    }
+
+
 }
