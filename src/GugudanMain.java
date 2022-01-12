9b57a8d45ec350ecd1d41adad7fa5b10b9d9a96f
jungminji <wjdalswl0215@gmail.com>
2022-01-12 16:38:25 +0900

    클래스 사용

--- src/GugudanMain.java
+++ src/GugudanMain.java
@@ -1,13 +1,11 @@
 public class GugudanMain {
     public static void main(String[] args) {
-        Gugudan gugudan = new Gugudan();
-
         for (int j = 2; j < 10; j++) {
             // Gugudan의 calculate와 print 메소드 활용해 구구단 구현하기
             // Gugudan.calculate(j)와 같이 메소드를 호출할 수 있음.
             System.out.println("----------" + j +"단 ----------");
-            int[] result = gugudan.calculate(j);
-            gugudan.print(result);
+            int[] result = Gugudan.calculate(j);
+            Gugudan.print(result);
         }
     }
 }
