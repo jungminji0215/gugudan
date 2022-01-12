bc9ab3f459c2683a3ba2a81090dd959234cd4151
jungminji <wjdalswl0215@gmail.com>
2022-01-12 16:38:03 +0900

    클래스 사용

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -1,12 +1,4 @@
 public class Gugudan {
-    public static void main(String[] args) {
-        for (int j = 2; j < 10; j++) {
-            // calculate와 print 메소드 활용해 구구단 구현하기
-            System.out.println("-------------" + j + "단-------------");
-            int[] result = calculate(j);
-            print(result);
-        }
-    }
 
     public static int[] calculate(int times) {
         int[] result = new int[9];
@@ -15,7 +7,6 @@ public class Gugudan {
             // 계산 결과를 배열에 저장
             result[i] = times * (i + 1);
         }
-
         return result;
     }
 
